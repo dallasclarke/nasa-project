56d3a003dd58ada94f27044526cfc70d147ca034
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-12-20 23:30:06 -0800

    NavBar somewhat done and router set

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./styles/app.scss";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 import PicOfTheDay from "./components/PicOfTheDay";
 import MarsPhotos from "./components/MarsPhotos"
+
 
 
 
@@ -12,8 +14,13 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <MarsPhotos /> 
-      {/* <PicOfTheDay /> */}
+      <Switch>
+        {/* <MarsPhotos />  */}
+        {/* <PicOfTheDay /> */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/picoftheday" component={PicOfTheDay} />
+        <Route exact path="/marsphotos" component={MarsPhotos} />
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,56 +1,148 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem"
+import Button from "@material-ui/core/Button";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   header: {
     background: "white",
   },
-  type: {
-    color: "black",
-    paddingRight: "1rem",
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    [theme.breakpoints.down("xs")]: {
+      flexGrow: 1,
+    },
+  },
+  headerOptions: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-evenly",
   },
 }));
 
-export default function DenseAppBar() {
+const Header = (props) => {
+  const { history } = props;
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClick = (pageURL) => {
+    history.push(pageURL);
+    setAnchorEl(null);
+  };
+
+  const handleButtonClick = (pageURL) => {
+    history.push(pageURL);
+  };
+
+  const menuItems = [
+    {
+      menuTitle: "Home",
+      pageURL: "/",
+    },
+    {
+      menuTitle: "Pic of the day",
+      pageURL: "/picoftheday",
+    },
+    {
+      menuTitle: "Mars Photos",
+      pageURL: "/marsphotos",
+    },
+  ];
 
   return (
     <div className={classes.root}>
       <AppBar className={classes.header} position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="transparent"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu>
-              <MenuItem onClick={handleclose}>Home</MenuItem>
-          </Menu>
-          <Typography className={classes.type} variant="h8" color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
             <img
               className="nasa-logo"
+              alt="nasa-logo"
               src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
               style={{ width: "110px", height: "55px" }}
             />
           </Typography>
+          {isMobile ? (
+            <>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                {menuItems.map((menuItem) => {
+                  const { menuTitle, pageURL } = menuItem;
+                  return (
+                    <MenuItem onClick={() => handleMenuClick(pageURL)}>
+                      {menuTitle}
+                    </MenuItem>
+                  );
+                })}
+              </Menu>
+            </>
+          ) : (
+            <div className={classes.headerOptions}>
+              <Button
+                variant="contained"
+                onClick={() => handleButtonClick("/")}
+              >
+                HOME
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => handleButtonClick("/picoftheday")}
+              >
+                Pic of the Day
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => handleButtonClick("/marsphotos")}
+              >
+                Mars Photos
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
   );
-}
+};
+
+export default withRouter(Header);
