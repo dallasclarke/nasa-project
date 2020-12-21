562d85340744b810f9a4681755ceed98c9a209fd
Dallas Clarke <dallas.ag.clarke@gmail.com>
2020-12-20 16:23:48 -0800

    Styled and got photos working. Carousel also working

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./styles/app.scss";
-import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import PicOfTheDay from "./components/PicOfTheDay";
 import MarsPhotos from "./components/MarsPhotos"
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <PicOfTheDay /> 
+      
+      <MarsPhotos /> 
+      {/* <PicOfTheDay /> */}
     </div>
   );
 }
--- src/components/MarsPhotos.js
+++ src/components/MarsPhotos.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 async function fetchMarPhotos() {
@@ -14,6 +18,18 @@ async function fetchMarPhotos() {
 }
 
 const MarsPhotos = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    className: 'the-slide',
+    
+  };
+
+
   const [marsData, setMarsData] = useState(null);
 
 
@@ -30,17 +46,19 @@ const MarsPhotos = () => {
 
   return (
     <div className="mars-photos">
+      <Slider {...settings}>
         {marsData.photos.map((photo) => {
           return (
             <div key={photo.id}>
               <h1>{photo.camera.name}</h1>
               <img
                 src={photo.img_src}
-                style={{ width: "30%", height: "30%" }}
+                style={{ width: "30%", height: "30%"}}
               />
             </div>
           );
         })}
+      </Slider>
     </div>
   );
 };
